1df8927d61493fba977776c1866fc44f2d393199
Ashraful <ashrafulrobin3@gmail.com>
2024-09-02 12:49:22 +0600

    Added socket layer but not working

--- src/api/routes.rs
+++ src/api/routes.rs
@@ -1,13 +1,30 @@
+use axum::http::header::{ACCEPT, AUTHORIZATION, ORIGIN};
+use axum::http::Method;
 use axum::{routing::get, Router};
 use deadpool_diesel::{Manager, Pool};
 use diesel::PgConnection;
+use log::info;
+use socketioxide::SocketIo;
+use tower_http::cors::{Any, CorsLayer};
 
 use crate::api::handlers::common_handlers;
 use crate::api::handlers::error_handlers;
+use crate::chat::services::soc_on_connect;
 
 pub fn create_api_router(pool: Pool<Manager<PgConnection>>) -> Router {
+    info!("Creating socket layer");
+    let (soc_layer, io) = SocketIo::new_layer();
+    io.ns("/", soc_on_connect);
+    
+    let cors_layer = CorsLayer::new()
+        .allow_methods(vec![Method::GET, Method::POST, Method::PUT, Method::DELETE])
+        .allow_headers(vec![ORIGIN, AUTHORIZATION, ACCEPT])
+        .allow_origin(Any);
+
     Router::new()
         .route("/", get(common_handlers::index))
+        .layer(cors_layer)
+        .layer(soc_layer)
         .fallback(error_handlers::handle_404)
         .with_state(pool)
 }
--- src/chat/services.rs
+++ src/chat/services.rs
@@ -1,66 +1,24 @@
-use axum::{extract::ws::Message, Extension};
+use log::info;
 use serde_json::Value;
-use socketioxide::{extract::{Data, SocketRef}, SocketIo};
+use socketioxide::extract::{AckSender, Bin, Data, SocketRef};
 
-use crate::chat::schema::UserCnt;
+pub async fn soc_on_connect(socket: SocketRef, Data(data): Data<Value>) {
+    info!("Socket.IO connected: {:?} {:?}", socket.ns(), socket.id);
+    socket.emit("auth", data).ok();
 
-pub async fn socket_layer() {
-    let (layer, io) = SocketIo::builder().with_state(UserCnt::new()).build_layer();
+    socket.on(
+        "message",
+        |socket: SocketRef, Data::<Value>(data), Bin(bin)| {
+            info!("Received event: {:?} {:?}", data, bin);
+            socket.bin(bin).emit("message-back", data).ok();
+        },
+    );
 
-    io.ns("/", |s: SocketRef| {
-        s.on(
-            "new message",
-            |s: SocketRef, Data::<Value>(msg), Extension::<Username>(username)| {
-                let msg = Res::Message {
-                    username,
-                    message: msg,
-                };
-                s.broadcast().emit("new message", msg).ok();
-            },
-        );
-
-        // s.on(
-        //     "add user",
-        //     |s: SocketRef, Data::<String>(username), user_cnt: State<UserCnt>| {
-        //         if s.extensions.get::<Username>().is_some() {
-        //             return;
-        //         }
-        //         let num_users = user_cnt.add_user();
-        //         s.extensions.insert(Username(username.clone()));
-        //         s.emit("login", Res::Login { num_users }).ok();
-
-        //         let res = Res::UserEvent {
-        //             num_users,
-        //             username: Username(username),
-        //         };
-        //         s.broadcast().emit("user joined", res).ok();
-        //     },
-        // );
-
-        // s.on("typing", |s: SocketRef, Extension::<Username>(username)| {
-        //     s.broadcast()
-        //         .emit("typing", Res::Username { username })
-        //         .ok();
-        // });
-
-        // s.on(
-        //     "stop typing",
-        //     |s: SocketRef, Extension::<Username>(username)| {
-        //         s.broadcast()
-        //             .emit("stop typing", Res::Username { username })
-        //             .ok();
-        //     },
-        // );
-
-        // s.on_disconnect(
-        //     |s: SocketRef, user_cnt: State<UserCnt>, Extension::<Username>(username)| {
-        //         let num_users = user_cnt.remove_user();
-        //         let res = Res::UserEvent {
-        //             num_users,
-        //             username,
-        //         };
-        //         s.broadcast().emit("user left", res).ok();
-        //     },
-        // );
-    });
+    socket.on(
+        "message-with-ack",
+        |Data::<Value>(data), ack: AckSender, Bin(bin)| {
+            info!("Received event: {:?} {:?}", data, bin);
+            ack.bin(bin).send(data).ok();
+        },
+    );
 }
